64ce60fca6ee8ff0551f41f3e965d95da15fb1b4
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-01-20 10:35:30 -0200

    Removed unused code
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- handlers/update.go
+++ handlers/update.go
@@ -1,25 +1,8 @@
 package handlers
 
 type InstallUpdateHandler interface {
-	PackageObjectInstaller
-}
-
-func InstallUpdate(h InstallUpdateHandler) error {
-	if err := h.CheckRequirements(); err != nil {
-		return err
-	}
-
-	if err := h.Setup(); err != nil {
-		return err
-	}
-
-	if err := h.Install(); err != nil {
-		return err
-	}
-
-	if err := h.Cleanup(); err != nil {
-		return err
-	}
-
-	return nil
+	CheckRequirements() error
+	Setup() error
+	Install() error
+	Cleanup() error
 }
