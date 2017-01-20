6432475fea5db42600b364dfb04545cd98158053
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-01-20 18:26:46 -0200

    Constructs Metadata object from JSON
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- metadata/json.go
+++ metadata/json.go
@@ -6,6 +6,41 @@ import (
 	"bitbucket.org/ossystems/agent/installmodes"
 )
 
+func FromJSON(bytes []byte) (*Metadata, error) {
+	var wrapper struct {
+		Metadata
+		RawObjects [][]interface{} `json:"objects"`
+	}
+
+	err := json.Unmarshal(bytes, &wrapper)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unwraps metadata
+	metadata := wrapper.Metadata
+
+	for _, list := range wrapper.RawObjects {
+		var objects []Object
+
+		for _, obj := range list {
+			// It is safe to ignore errors here
+			b, _ := json.Marshal(obj)
+
+			o, err := ObjectFromJSON(b)
+			if err != nil {
+				return nil, err
+			}
+
+			objects = append(objects, o)
+		}
+
+		metadata.Objects = append(metadata.Objects, objects)
+	}
+
+	return &metadata, nil
+}
+
 func ObjectFromJSON(bytes []byte) (Object, error) {
 	var v map[string]interface{}
 
--- metadata/json_test.go
+++ metadata/json_test.go
@@ -7,11 +7,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	validJSONMetadata = `{
+	  "product-uid": "0123456789",
+	  "objects": [
+	    [
+	      { "mode": "test" }
+	    ]
+	  ]
+	}`
+)
+
 type TestObject struct {
 	Object
 }
 
-func TestObjectFROMJson(t *testing.T) {
+func TestMetadataFromValidJson(t *testing.T) {
+	installmodes.RegisterInstallMode("test", installmodes.InstallMode{
+		Mode:              "test",
+		CheckRequirements: func() error { return nil },
+		Instantiate:       func() interface{} { return TestObject{} },
+	})
+
+	m, err := FromJSON([]byte(validJSONMetadata))
+	if !assert.NotNil(t, m) {
+		t.Fatal(err)
+	}
+
+	assert.NotEmpty(t, m.Objects)
+	assert.NotEmpty(t, m.Objects[0])
+	assert.IsType(t, TestObject{}, m.Objects[0][0])
+}
+
+func TestObjectFromValidJson(t *testing.T) {
 	installmodes.RegisterInstallMode("test", installmodes.InstallMode{
 		Mode:              "test",
 		CheckRequirements: func() error { return nil },
