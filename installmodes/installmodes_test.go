9d9d1f78c8ce77b86a7bf1abde90485f5cbce365
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-01-20 15:23:57 -0200

    Add more tests
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- installmodes/installmodes_test.go
+++ installmodes/installmodes_test.go
@@ -7,8 +7,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type TestObject struct {
+}
+
+func TestRegisterInstallMode(t *testing.T) {
+	RegisterInstallMode("test1", InstallMode{
+		CheckRequirements: func() error { return nil },
+		Instantiate:       func() interface{} { return &TestObject{} },
+	})
+
+	obj, err := GetObject("test1")
+	if !assert.NotNil(t, obj) {
+		t.Fatal(err)
+	}
+}
+
+func TestGetObjectNotFound(t *testing.T) {
+	_, err := GetObject("")
+
+	if assert.Error(t, err) {
+		assert.Equal(t, errors.New("Object not found"), err)
+	}
+}
+
 func TestCheckRequirements(t *testing.T) {
-	RegisterInstallMode("test", InstallMode{
+	RegisterInstallMode("test2", InstallMode{
 		CheckRequirements: func() error { return nil },
 	})
 
@@ -17,7 +40,7 @@ func TestCheckRequirements(t *testing.T) {
 }
 
 func TestFailCheckRequirements(t *testing.T) {
-	RegisterInstallMode("test", InstallMode{
+	RegisterInstallMode("test3", InstallMode{
 		CheckRequirements: func() error { return errors.New("") },
 	})
 
