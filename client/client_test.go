19d92e63ff7108f2e0ec54b62697cdd0f398f55f
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-01-25 14:56:37 -0200

    Add client api tests
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- client/client_test.go
+++ client/client_test.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,6 +12,40 @@ func TestNewApiClient(t *testing.T) {
 	c := NewApiClient("localhost")
 	assert.NotNil(t, c)
 	assert.Equal(t, "localhost", c.server)
+}
+
+func TestApiClientRequest(t *testing.T) {
+	c := NewApiClient("localhost")
+	assert.NotNil(t, c)
+
+	req := c.Request()
+	assert.NotNil(t, req)
+
+	assert.Equal(t, c, req.Client())
+
+	responder := &struct {
+		httpStatus int
+		headers    http.Header
+	}{
+		http.StatusOK,
+		http.Header{},
+	}
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		responder.headers = r.Header
+		w.WriteHeader(responder.httpStatus)
+		w.Header().Set("Content-Type", "application/json")
+	}))
+
+	defer s.Close()
+
+	hreq, _ := http.NewRequest(http.MethodGet, s.URL, nil)
+
+	res, err := req.Do(hreq)
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	assert.NotNil(t, responder.headers)
+	assert.Equal(t, responder.httpStatus, res.StatusCode)
 }
 
 func TestServerURL(t *testing.T) {
