0be03fdd08a49bf87c6e02ebdf76866cc966712d
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-01-27 11:13:53 -0200

    metadata: add Checksum method
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,7 +1,40 @@
 package metadata
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+)
+
 type Metadata struct {
 	ProductUID string     `json:"product-uid"`
 	Version    string     `json:"version"`
 	Objects    [][]Object `json:"-"`
+}
+
+func (m Metadata) Checksum() (string, error) {
+	var wrapper struct {
+		Metadata
+		Objects [][]Object `json:"objects"`
+	}
+
+	wrapper.Metadata = m
+	wrapper.Objects = m.Objects
+
+	data, err := json.Marshal(wrapper)
+	if err != nil {
+		return "", err
+	}
+
+	r := bytes.NewReader(data)
+
+	hash := sha256.New()
+	_, err = io.Copy(hash, r)
+	if err != nil {
+		return "", nil
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
 }
