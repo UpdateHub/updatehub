4e6b3ed2b37ddf63db3f589a5d76846fbf85bb0d
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-01-31 18:28:06 -0200

    Add ini Settings
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

---
+++ settings.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"io"
+
+	"github.com/go-ini/ini"
+)
+
+const (
+	defaultPollingInterval = 60 * 60 // one hour (in seconds)
+)
+
+type Settings struct {
+	PollingSettings  `ini:"Polling"`
+	StorageSettings  `ini:"Storage"`
+	UpdateSettings   `ini:"Update"`
+	NetworkSettings  `ini:"Network"`
+	FirmwareSettings `ini:"Firmware"`
+}
+
+type PollingSettings struct {
+	PollingInterval      int  `ini:"Interval,omitempty"`
+	PollingEnabled       bool `ini:"Enabled,omitempty"`
+	LastPoll             int  `ini:"LastPoll"`
+	FirstPoll            int  `ini:"FirstPoll"`
+	ExtraPollingInterval int  `ini:"ExtraInterval"`
+	PollingRetries       int  `ini:"Retries"`
+}
+
+type StorageSettings struct {
+	ReadOnly bool `ini:"ReadOnly"`
+}
+
+type UpdateSettings struct {
+	DownloadDir               string   `ini:"DownloadDir"`
+	AutoDownloadWhenAvailable bool     `ini:"AutoDownloadWhenAvailable"`
+	AutoInstallAfterDownload  bool     `ini:"AutoInstallAfterDownload"`
+	AutoRebootAfterInstall    bool     `ini:"AutoRebootAfterInstall"`
+	SupportedInstallModes     []string `ini:"SupportedInstallModes"`
+}
+
+type NetworkSettings struct {
+	DisableHTTPS  bool   `ini:"DisableHttps"`
+	ServerAddress string `ini:"EasyFotaServerAddress"`
+}
+
+type FirmwareSettings struct {
+	FirmwareMetadataPath string `ini:"MetadataPath"`
+}
+
+func init() {
+	ini.PrettyFormat = false
+}
+
+func NewSettings(data []byte) (*Settings, error) {
+	cfg, err := ini.Load(data)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Settings{
+		PollingSettings: PollingSettings{
+			PollingInterval:      defaultPollingInterval,
+			PollingEnabled:       true,
+			LastPoll:             0,
+			FirstPoll:            0,
+			ExtraPollingInterval: 0,
+			PollingRetries:       0,
+		},
+
+		StorageSettings: StorageSettings{
+			ReadOnly: false,
+		},
+
+		UpdateSettings: UpdateSettings{
+			DownloadDir:               "/tmp",
+			AutoDownloadWhenAvailable: true,
+			AutoInstallAfterDownload:  true,
+			AutoRebootAfterInstall:    true,
+			SupportedInstallModes:     []string{"dry-run", "copy", "flash", "imxkobs", "raw", "tarball", "ubifs"},
+		},
+
+		NetworkSettings: NetworkSettings{
+			DisableHTTPS:  false,
+			ServerAddress: "",
+		},
+
+		FirmwareSettings: FirmwareSettings{
+			FirmwareMetadataPath: "",
+		},
+	}
+
+	err = cfg.MapTo(s)
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+func SaveSettings(s *Settings, w io.Writer) error {
+	cfg := ini.Empty()
+
+	err := ini.ReflectFrom(cfg, s)
+	if err != nil {
+		return err
+	}
+
+	_, err = cfg.WriteTo(w)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ settings_test.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const customSettings = `
+[Polling]
+Interval=1
+Enabled=false
+LastPoll=2
+FirstPoll=3
+ExtraInterval=4
+Retries=5
+
+[Storage]
+ReadOnly=true
+
+[Update]
+DownloadDir=/tmp/download
+AutoDownloadWhenAvailable=false
+AutoInstallAfterDownload=false
+AutoRebootAfterInstall=false
+SupportedInstallModes=mode1,mode2
+
+[Network]
+DisableHttps=true
+EasyFotaServerAddress=localhost
+
+[Firmware]
+MetadataPath=/tmp/metadata
+`
+
+func TestNewSettings(t *testing.T) {
+	testCases := []struct {
+		name             string
+		data             string
+		expectedSettings *Settings
+	}{
+		{
+			"DefaultValues",
+			"",
+			&Settings{
+				PollingSettings: PollingSettings{
+					PollingInterval:      defaultPollingInterval,
+					PollingEnabled:       true,
+					LastPoll:             0,
+					FirstPoll:            0,
+					ExtraPollingInterval: 0,
+					PollingRetries:       0,
+				},
+
+				StorageSettings: StorageSettings{
+					ReadOnly: false,
+				},
+
+				UpdateSettings: UpdateSettings{
+					DownloadDir:               "/tmp",
+					AutoDownloadWhenAvailable: true,
+					AutoInstallAfterDownload:  true,
+					AutoRebootAfterInstall:    true,
+					SupportedInstallModes:     []string{"dry-run", "copy", "flash", "imxkobs", "raw", "tarball", "ubifs"},
+				},
+
+				NetworkSettings: NetworkSettings{
+					DisableHTTPS:  false,
+					ServerAddress: "",
+				},
+
+				FirmwareSettings: FirmwareSettings{
+					FirmwareMetadataPath: "",
+				},
+			},
+		},
+
+		{
+			"CustomValues",
+			customSettings,
+			&Settings{
+				PollingSettings: PollingSettings{
+					PollingInterval:      1,
+					PollingEnabled:       false,
+					LastPoll:             2,
+					FirstPoll:            3,
+					ExtraPollingInterval: 4,
+					PollingRetries:       5,
+				},
+
+				StorageSettings: StorageSettings{
+					ReadOnly: true,
+				},
+
+				UpdateSettings: UpdateSettings{
+					DownloadDir:               "/tmp/download",
+					AutoDownloadWhenAvailable: false,
+					AutoInstallAfterDownload:  false,
+					AutoRebootAfterInstall:    false,
+					SupportedInstallModes:     []string{"mode1", "mode2"},
+				},
+
+				NetworkSettings: NetworkSettings{
+					DisableHTTPS:  true,
+					ServerAddress: "localhost",
+				},
+
+				FirmwareSettings: FirmwareSettings{
+					FirmwareMetadataPath: "/tmp/metadata",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			s, err := NewSettings([]byte(tc.data))
+			assert.NoError(t, err)
+			assert.NotNil(t, s)
+			assert.Equal(t, tc.expectedSettings, s)
+		})
+	}
+}
+
+func TestSaveSettings(t *testing.T) {
+	expectedSettings, err := NewSettings([]byte(""))
+	assert.NoError(t, err)
+	assert.NotNil(t, expectedSettings)
+
+	var buf bytes.Buffer
+	err = SaveSettings(expectedSettings, &buf)
+	assert.NoError(t, err)
+
+	s, err := NewSettings(buf.Bytes())
+	assert.NoError(t, err)
+	assert.NotNil(t, s)
+
+	assert.Equal(t, expectedSettings, s)
+}
