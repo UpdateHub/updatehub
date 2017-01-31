d5a6b083538d523f4e0e9689182f7912b29fe174
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-01-31 09:23:22 -0200

    add tests for FetchUpdate
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- easyfota_test.go
+++ easyfota_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"io"
+	"io/ioutil"
+	"path"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"bitbucket.org/ossystems/agent/client"
+	"bitbucket.org/ossystems/agent/installmodes"
 	"bitbucket.org/ossystems/agent/metadata"
 )
 
@@ -15,6 +19,8 @@ type UpdaterTest struct {
 	updateMetadata   *metadata.UpdateMetadata
 	extraPoll        int
 	checkUpdateError error
+	fetchUpdateError error
+	updateBytes      []byte
 }
 
 func (u UpdaterTest) CheckUpdate(api client.ApiRequester) (interface{}, int, error) {
@@ -22,12 +28,21 @@ func (u UpdaterTest) CheckUpdate(api client.ApiRequester) (interface{}, int, err
 }
 
 func (u UpdaterTest) FetchUpdate(api client.ApiRequester, uri string) (io.ReadCloser, int64, error) {
-	return nil, 0, nil
+	rd := bytes.NewReader(u.updateBytes)
+	return ioutil.NopCloser(rd), 0, nil
+}
+
+type TestObject struct {
+	metadata.ObjectMetadata
 }
 
 const validUpdateMetadata = `{
   "product-uid": "123",
-  "objects": [ [ ] ]
+  "objects": [
+    [
+      { "mode": "test", "sha256sum": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" }
+    ]
+  ]
 }`
 
 var checkUpdateTestCases = []struct {
@@ -75,4 +90,35 @@ func TestEasyfotaCheckUpdate(t *testing.T) {
 			assert.Equal(t, tc.extraPoll, extraPoll)
 		})
 	}
+}
+
+func TestEasyFotaFetchUpdate(t *testing.T) {
+	installmodes.RegisterInstallMode("test", installmodes.InstallMode{
+		CheckRequirements: func() error { return nil },
+		GetObject:         func() interface{} { return &TestObject{} },
+	})
+
+	fota := &EasyFota{
+		state:    &PollState{},
+		timeStep: time.Millisecond,
+		api:      client.NewApiClient("localhost"),
+	}
+
+	updateMetadata, err := metadata.FromJSON([]byte(validUpdateMetadata))
+	assert.NoError(t, err)
+
+	updater := UpdaterTest{
+		updateMetadata: updateMetadata,
+		extraPoll:      0,
+		updateBytes:    []byte("0123456789"),
+	}
+
+	fota.updater = client.Updater(updater)
+
+	err = fota.FetchUpdate(updateMetadata, nil)
+	assert.NoError(t, err)
+
+	data, err := ioutil.ReadFile(path.Join("/tmp", updateMetadata.Objects[0][0].GetObjectMetadata().Sha256sum))
+	assert.NoError(t, err)
+	assert.Equal(t, updater.updateBytes, data)
 }
