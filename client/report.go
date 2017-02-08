d5fe95390450c6e96c5bf0e7f2203544632b56e0
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-02-08 10:06:28 -0200

    client api: Add report interface
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

---
+++ client/report.go
@@ -0,0 +1,56 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+type ReportClient struct {
+}
+
+type Reporter interface {
+	ReportState(api ApiRequester, packageUID string, state string) error
+}
+
+func (u *ReportClient) ReportState(api ApiRequester, packageUID string, state string) error {
+	if api == nil {
+		return errors.New("invalid api requester")
+	}
+
+	url := serverURL(api.Client(), StateReportEndpoint)
+
+	data := make(map[string]interface{})
+	data["status"] = state
+	data["package-uid"] = packageUID
+	data["error-message"] = ""
+
+	body, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
+	if err != nil {
+		return errors.New("failed to create report request")
+	}
+
+	res, err := api.Do(req)
+	if err != nil {
+		return errors.New("report request failed")
+	}
+
+	defer res.Body.Close()
+
+	switch res.StatusCode {
+	case http.StatusOK:
+		return nil
+	}
+
+	return errors.New("failed to report state")
+}
+
+func NewReportClient() *ReportClient {
+	return &ReportClient{}
+}
---
+++ client/report_test.go
@@ -0,0 +1,73 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReportState(t *testing.T) {
+	responder := &struct {
+		httpStatus int
+		headers    http.Header
+		body       string
+	}{
+		http.StatusOK,
+		http.Header{},
+		"",
+	}
+
+	rawBody := []byte{}
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		responder.headers = r.Header
+		w.WriteHeader(responder.httpStatus)
+		w.Header().Set("Content-Type", "application/json")
+
+		fmt.Fprintf(w, responder.body)
+
+		buf := new(bytes.Buffer)
+
+		n, err := buf.ReadFrom(r.Body)
+		assert.NoError(t, err)
+		assert.NotZero(t, n)
+
+		rawBody = buf.Bytes()
+	}))
+
+	defer s.Close()
+
+	url, err := url.Parse(s.URL)
+	assert.NoError(t, err)
+
+	c := NewApiClient(url.Host)
+	assert.NotNil(t, c)
+
+	req := c.Request()
+	assert.NotNil(t, req)
+
+	assert.Equal(t, c, req.Client())
+
+	reporter := NewReportClient()
+
+	err = reporter.ReportState(c.Request(), "packageUID", "state")
+	assert.NoError(t, err)
+
+	var body map[string]interface{}
+
+	err = json.Unmarshal(rawBody, &body)
+	assert.NoError(t, err)
+
+	expectedBody := make(map[string]interface{})
+	expectedBody["error-message"] = ""
+	expectedBody["package-uid"] = "packageUID"
+	expectedBody["status"] = "state"
+
+	assert.Equal(t, expectedBody, body)
+}
