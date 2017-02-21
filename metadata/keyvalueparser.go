204d9c351bb14c5c9464dcdda846f4f6d6f0c127
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-02-21 16:04:20 -0300

    Add keyvalue parser
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

---
+++ metadata/keyvalueparser.go
@@ -0,0 +1,34 @@
+package metadata
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func keyValueParser(r io.Reader) (map[string]string, error) {
+	b := bufio.NewReader(r)
+
+	keyvalue := make(map[string]string)
+
+	l := 0
+
+	for {
+		line, _, err := b.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		l++
+
+		parts := strings.SplitN(string(line), "=", 2)
+		if len(parts) < 2 {
+			return nil, fmt.Errorf("'=' expected on line %d", l)
+		}
+
+		keyvalue[parts[0]] = parts[1]
+	}
+
+	return keyvalue, nil
+}
---
+++ metadata/keyvalueparser_test.go
@@ -0,0 +1,39 @@
+package metadata
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKeyValueParser(t *testing.T) {
+	expected := map[string]string{
+		"key":         "value",
+		"another_key": "value",
+	}
+
+	keyvalue, err := keyValueParser(bytes.NewReader([]byte("key=value\nanother_key=value")))
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, keyvalue)
+}
+
+func TestKeyValueParserWithInvalidKey(t *testing.T) {
+	keyvalue, err := keyValueParser(bytes.NewReader([]byte("key=value\nanother_key=value\ninvalid_key")))
+
+	assert.EqualError(t, err, "'=' expected on line 3")
+	assert.Nil(t, keyvalue)
+}
+
+func TestKeyValueParserWithEmptyKey(t *testing.T) {
+	expected := map[string]string{
+		"key":       "value",
+		"empty_key": "",
+	}
+
+	keyvalue, err := keyValueParser(bytes.NewReader([]byte("key=value\nempty_key=")))
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, keyvalue)
+}
