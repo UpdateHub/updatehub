db96d9e35e19bad706271cd76aef291b9a09e63a
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-02-22 10:28:16 -0300

    Execute hooks of device-identity and device-attributes
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- metadata/firmware.go
+++ metadata/firmware.go
@@ -1,45 +1,99 @@
 package metadata
 
-import "bitbucket.org/ossystems/agent/utils"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path"
+	"syscall"
+
+	"github.com/spf13/afero"
+
+	"bitbucket.org/ossystems/agent/utils"
+)
 
 type FirmwareMetadata struct {
-	ProductUID       string                 `json:"product-uid"`
-	DeviceIdentity   map[string]interface{} `json:"device-identity"`
-	Version          string                 `json:"version"`
-	Hardware         string                 `json:"hardware"`
-	HardwareRevision string                 `json:"hardware-revision"`
-	DeviceAttributes map[string]interface{} `json:"device-attributes"`
+	ProductUID       string            `json:"product-uid"`
+	DeviceIdentity   map[string]string `json:"device-identity"`
+	Version          string            `json:"version"`
+	Hardware         string            `json:"hardware"`
+	HardwareRevision string            `json:"hardware-revision"`
+	DeviceAttributes map[string]string `json:"device-attributes"`
 }
 
-func NewFirmwareMetadata(cmd utils.CmdLineExecuter) (*FirmwareMetadata, error) {
+func NewFirmwareMetadata(basePath string, store afero.Fs, cmd utils.CmdLineExecuter) (*FirmwareMetadata, error) {
 	// FIXME: DeviceIdentity and DeviceAttributes
 
-	productUID, err := cmd.Execute("product-uid")
+	productUID, err := cmd.Execute(path.Join(basePath, "product-uid"))
 	if err != nil {
 		return nil, err
 	}
 
-	hardware, err := cmd.Execute("hardware")
+	hardware, err := cmd.Execute(path.Join(basePath, "hardware"))
 	if err != nil {
 		return nil, err
 	}
 
-	hardwareRevision, err := cmd.Execute("hardware-revision")
+	hardwareRevision, err := cmd.Execute(path.Join(basePath, "hardware-revision"))
 	if err != nil {
 		return nil, err
 	}
 
-	version, err := cmd.Execute("version")
+	version, err := cmd.Execute(path.Join(basePath, "version"))
+	if err != nil {
+		return nil, err
+	}
+
+	deviceIdentity, err := executeHooks(path.Join(basePath, "device-identity.d"), store, cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	deviceAttributes, err := executeHooks(path.Join(basePath, "device-attributes.d"), store, cmd)
 	if err != nil {
 		return nil, err
 	}
 
 	firmwareMetadata := &FirmwareMetadata{
 		ProductUID:       string(productUID),
+		DeviceIdentity:   deviceIdentity,
+		DeviceAttributes: deviceAttributes,
 		Hardware:         string(hardware),
 		HardwareRevision: string(hardwareRevision),
 		Version:          string(version),
 	}
 
 	return firmwareMetadata, nil
+}
+
+func executeHooks(basePath string, store afero.Fs, cmd utils.CmdLineExecuter) (map[string]string, error) {
+	files, err := afero.ReadDir(store, basePath)
+	if err != nil && !os.IsNotExist(err) {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	keyValueMap := map[string]string{}
+
+	for _, file := range files {
+		if file.IsDir() || file.Mode()&syscall.S_IXUSR == 0 {
+			continue
+		}
+
+		output, err := cmd.Execute(path.Join(basePath, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		keyValue, err := keyValueParser(bytes.NewReader(output))
+		if err != nil {
+			return nil, err
+		}
+
+		for k, v := range keyValue {
+			keyValueMap[k] = v
+		}
+	}
+
+	return keyValueMap, nil
 }
--- metadata/firmware_test.go
+++ metadata/firmware_test.go
@@ -3,27 +3,61 @@ package metadata
 import (
 	"testing"
 
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
 type firmwareMetadataCmdExecuter struct {
+	store afero.Fs
 }
 
 func (exe firmwareMetadataCmdExecuter) Execute(cmdline string) ([]byte, error) {
+	if _, err := exe.store.Stat(cmdline); err == nil {
+		data, _ := afero.ReadFile(exe.store, cmdline)
+		return data, nil
+	}
+
 	return []byte(cmdline), nil
 }
 
 func TestNewFirmwareMetadata(t *testing.T) {
 	expected := &FirmwareMetadata{
-		ProductUID:       "product-uid",
-		Hardware:         "hardware",
-		HardwareRevision: "hardware-revision",
-		Version:          "version",
+		ProductUID: "/product-uid",
+		DeviceIdentity: map[string]string{
+			"id1": "value",
+			"id2": "value",
+		},
+		DeviceAttributes: map[string]string{
+			"attr1": "value",
+			"attr2": "value",
+		},
+		Hardware:         "/hardware",
+		HardwareRevision: "/hardware-revision",
+		Version:          "/version",
 	}
 
-	exe := firmwareMetadataCmdExecuter{}
+	store := afero.NewMemMapFs()
 
-	firmwareMetadata, err := NewFirmwareMetadata(exe)
+	exe := firmwareMetadataCmdExecuter{
+		store: store,
+	}
+
+	files := map[string]string{
+		"/device-identity.d/key1":    "id1=value",
+		"/device-identity.d/key2":    "id2=value",
+		"/device-attributes.d/attr1": "attr1=value",
+		"/device-attributes.d/attr2": "attr2=value",
+	}
+
+	err := store.MkdirAll("/device-identity.d/", 0755)
+	assert.NoError(t, err)
+
+	for k, v := range files {
+		err := afero.WriteFile(store, k, []byte(v), 0700)
+		assert.NoError(t, err)
+	}
+
+	firmwareMetadata, err := NewFirmwareMetadata("/", store, exe)
 
 	assert.NoError(t, err)
 	assert.Equal(t, expected, firmwareMetadata)
