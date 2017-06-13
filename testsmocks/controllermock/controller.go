dc1de4bd142bec9b275508cfc86d0a040e1eedd9
Giulian Gonçalves Vivan <giulian@ossystems.com.br>
2017-06-13 13:39:11 -0300

    HTTP API "/update" route implementation
    Signed-off-by: Giulian Gonçalves Vivan <giulian@ossystems.com.br>

---
+++ testsmocks/controllermock/controller.go
@@ -0,0 +1,40 @@
+/*
+ * UpdateHub
+ * Copyright (C) 2017
+ * O.S. Systems Sofware LTDA: contato@ossystems.com.br
+ *
+ * SPDX-License-Identifier:     GPL-2.0
+ */
+
+package controllermock
+
+import (
+	"time"
+
+	"github.com/UpdateHub/updatehub/metadata"
+	"github.com/stretchr/testify/mock"
+)
+
+type ControllerMock struct {
+	mock.Mock
+}
+
+func (cm *ControllerMock) CheckUpdate(retries int) (*metadata.UpdateMetadata, time.Duration) {
+	args := cm.Called(retries)
+	return args.Get(0).(*metadata.UpdateMetadata), args.Get(1).(time.Duration)
+}
+
+func (cm *ControllerMock) FetchUpdate(updateMetadata *metadata.UpdateMetadata, cancel <-chan bool, progressChan chan<- int) error {
+	args := cm.Called(updateMetadata, cancel, progressChan)
+	return args.Error(0)
+}
+
+func (cm *ControllerMock) InstallUpdate(updateMetadata *metadata.UpdateMetadata, progressChan chan<- int) error {
+	args := cm.Called(updateMetadata, progressChan)
+	return args.Error(0)
+}
+
+func (cm *ControllerMock) ReportCurrentState() error {
+	args := cm.Called()
+	return args.Error(0)
+}
---
+++ testsmocks/controllermock/controller_test.go
@@ -0,0 +1,77 @@
+/*
+ * UpdateHub
+ * Copyright (C) 2017
+ * O.S. Systems Sofware LTDA: contato@ossystems.com.br
+ *
+ * SPDX-License-Identifier:     GPL-2.0
+ */
+
+package controllermock
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/UpdateHub/updatehub/metadata"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckUpdate(t *testing.T) {
+	expectedMetadata := &metadata.UpdateMetadata{}
+	expectedDuration := 10 * time.Second
+
+	cm := &ControllerMock{}
+	cm.On("CheckUpdate", 0).Return(expectedMetadata, expectedDuration)
+
+	m, d := cm.CheckUpdate(0)
+
+	assert.Equal(t, expectedMetadata, m)
+	assert.Equal(t, expectedDuration, d)
+
+	cm.AssertExpectations(t)
+}
+
+func TestFetchUpdate(t *testing.T) {
+	expectedError := fmt.Errorf("some error")
+	metadata := &metadata.UpdateMetadata{}
+	cancelChannel := make(<-chan bool)
+	progressChannel := make(chan<- int)
+
+	cm := &ControllerMock{}
+	cm.On("FetchUpdate", metadata, cancelChannel, progressChannel).Return(expectedError)
+
+	err := cm.FetchUpdate(metadata, cancelChannel, progressChannel)
+
+	assert.Equal(t, expectedError, err)
+
+	cm.AssertExpectations(t)
+}
+
+func TestInstallUpdate(t *testing.T) {
+	expectedError := fmt.Errorf("some error")
+	metadata := &metadata.UpdateMetadata{}
+	progressChannel := make(chan<- int)
+
+	cm := &ControllerMock{}
+	cm.On("InstallUpdate", metadata, progressChannel).Return(expectedError)
+
+	err := cm.InstallUpdate(metadata, progressChannel)
+
+	assert.Equal(t, expectedError, err)
+
+	cm.AssertExpectations(t)
+}
+
+func TestReportCurrentState(t *testing.T) {
+	expectedError := fmt.Errorf("some error")
+
+	cm := &ControllerMock{}
+	cm.On("ReportCurrentState").Return(expectedError)
+
+	err := cm.ReportCurrentState()
+
+	assert.Equal(t, expectedError, err)
+
+	cm.AssertExpectations(t)
+}
