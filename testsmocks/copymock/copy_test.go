90522c8cc495dadfa651af3ad208d1ef20792a77
Giulian Gonçalves Vivan <giulian@ossystems.com.br>
2017-07-07 16:20:34 -0300

    copymock tests
    Signed-off-by: Giulian Gonçalves Vivan <giulian@ossystems.com.br>

---
+++ testsmocks/copymock/copy_test.go
@@ -0,0 +1,81 @@
+/*
+ * UpdateHub
+ * Copyright (C) 2017
+ * O.S. Systems Sofware LTDA: contato@ossystems.com.br
+ *
+ * SPDX-License-Identifier:     GPL-2.0
+ */
+
+package copymock
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/UpdateHub/updatehub/testsmocks/filesystemmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCopyFile(t *testing.T) {
+	expectedError := fmt.Errorf("some error")
+	chunkSize := 256
+	skip := 2
+	seek := 3
+	count := 5
+	truncate := true
+	compressed := false
+
+	fsm := &filesystemmock.FileSystemBackendMock{}
+	cm := &CopyMock{}
+	cm.On("CopyFile", fsm, nil, "sourcepath", "targetpath", chunkSize, skip, seek, count, truncate, compressed).Return(expectedError)
+
+	err := cm.CopyFile(fsm, nil, "sourcepath", "targetpath", chunkSize, skip, seek, count, truncate, compressed)
+
+	assert.Equal(t, expectedError, err)
+
+	cm.AssertExpectations(t)
+	fsm.AssertExpectations(t)
+}
+
+func TestCopyToProcessStdin(t *testing.T) {
+	expectedError := fmt.Errorf("some error")
+	compressed := false
+
+	fsm := &filesystemmock.FileSystemBackendMock{}
+	cm := &CopyMock{}
+	cm.On("CopyToProcessStdin", fsm, nil, "sourcepath", "cmdline", compressed).Return(expectedError)
+
+	err := cm.CopyToProcessStdin(fsm, nil, "sourcepath", "cmdline", compressed)
+
+	assert.Equal(t, expectedError, err)
+
+	cm.AssertExpectations(t)
+	fsm.AssertExpectations(t)
+}
+
+func TestCopy(t *testing.T) {
+	expectedError := fmt.Errorf("some error")
+	wr := ioutil.Discard
+	rd := strings.NewReader("")
+	chunkSize := 256
+	skip := 2
+	count := 5
+	compressed := false
+	cancel := make(<-chan bool)
+	timeout := time.Minute
+
+	fsm := &filesystemmock.FileSystemBackendMock{}
+	cm := &CopyMock{}
+	cm.On("Copy", wr, rd, timeout, cancel, chunkSize, skip, count, compressed).Return(false, expectedError)
+
+	b, err := cm.Copy(wr, rd, timeout, cancel, chunkSize, skip, count, compressed)
+
+	assert.Equal(t, false, b)
+	assert.Equal(t, expectedError, err)
+
+	cm.AssertExpectations(t)
+	fsm.AssertExpectations(t)
+}
