ecf25649d40fd2c97c65ba47d3ca1a17fbb8a824
Giulian Gonçalves Vivan <giulian@ossystems.com.br>
2017-07-10 14:22:56 -0300

    statesmock tests
    Signed-off-by: Giulian Gonçalves Vivan <giulian@ossystems.com.br>

---
+++ testsmocks/statesmock/sha256checker_test.go
@@ -0,0 +1,31 @@
+/*
+ * UpdateHub
+ * Copyright (C) 2017
+ * O.S. Systems Sofware LTDA: contato@ossystems.com.br
+ *
+ * SPDX-License-Identifier:     GPL-2.0
+ */
+
+package statesmock
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckDownloadedObjectSha256sum(t *testing.T) {
+	fs := afero.NewMemMapFs()
+	expectedError := fmt.Errorf("some error")
+
+	scm := &Sha256CheckerMock{}
+	scm.On("CheckDownloadedObjectSha256sum", fs, "downloaddir", "sha256sum").Return(expectedError)
+
+	err := scm.CheckDownloadedObjectSha256sum(fs, "downloaddir", "sha256sum")
+
+	assert.Equal(t, expectedError, err)
+
+	scm.AssertExpectations(t)
+}
