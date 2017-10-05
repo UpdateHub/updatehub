975be9871ed6dbcdf45705702f7bc6c3eb066cf1
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-10-05 14:55:21 -0300

    Add func to determine transition flow for state change
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

---
+++ updatehub/transition_test.go
@@ -0,0 +1,43 @@
+/*
+ * UpdateHub
+ * Copyright (C) 2017
+ * O.S. Systems Sofware LTDA: contato@ossystems.com.br
+ *
+ * SPDX-License-Identifier:     GPL-2.0
+ */
+
+package updatehub
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransitionFlowUnchanged(t *testing.T) {
+	flow, args := DetermineTransitionFlow([]byte(""))
+
+	assert.Equal(t, TransitionFlow(TransitionFlowUnchanged), flow)
+	assert.Nil(t, args)
+}
+
+func TestTransitionFlowCancelled(t *testing.T) {
+	flow, args := DetermineTransitionFlow([]byte("cancel"))
+
+	assert.Equal(t, TransitionFlow(TransitionFlowCancelled), flow)
+	assert.Nil(t, args)
+}
+
+func TestTransitionFlowPostponed(t *testing.T) {
+	flow, args := DetermineTransitionFlow([]byte("try_again 13"))
+
+	assert.Equal(t, TransitionFlow(TransitionFlowPostponed), flow)
+	assert.Equal(t, args.(int), 13)
+}
+
+func TestTransitionFlowPostponedWithInvalidArgument(t *testing.T) {
+	flow, args := DetermineTransitionFlow([]byte("try_again x"))
+
+	assert.Equal(t, TransitionFlow(TransitionFlowUnchanged), flow)
+	assert.Nil(t, args)
+}
