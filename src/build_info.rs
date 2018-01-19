2c8ce7eeae111451b33c710e7e53a5b4be150506
Otavio Salvador <otavio@ossystems.com.br>
2018-01-19 17:31:25 -0200

    build_info: Add documentation
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/build_info.rs
+++ src/build_info.rs
@@ -3,8 +3,22 @@
 // SPDX-License-Identifier: GPL-2.0
 // 
 
+//! Build information module
+
 const VERSION: &str = env!("VERSION");
 
+/// Returns the version in use, including the commit and if there is
+/// uncommited modification in the source.
+///
+/// Internally, it use `git describe` to get the version and the
+/// number of changes since the last tag.
+///
+/// # Example
+/// ```
+/// use updatehub::build_info;
+///
+/// println!("Running version: {}", build_info::version());
+/// ```
 pub fn version() -> &'static str {
     VERSION
 }
