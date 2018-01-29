035afc1942a030e0dd1fc14a8da43c9634e5f584
Otavio Salvador <otavio@ossystems.com.br>
2018-01-29 17:13:59 -0200

    firmware: tests: Move to a sub-module
    The new sub-module will allow the grow of the tests because we
    will need to add helpers to use from other parts of the system and we
    wish it to be exclusively add when tests are in use.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ src/firmware/tests.rs
@@ -0,0 +1,107 @@
+use super::*;
+use mktemp::Temp;
+
+fn create_hook(path: &Path, contents: &str, mode: u32) {
+    use std::fs::File;
+    use std::fs::create_dir_all;
+    use std::fs::metadata;
+    use std::io::Write;
+    use std::os::unix::fs::PermissionsExt;
+
+    // ensure path exists
+    create_dir_all(path.parent().unwrap()).unwrap();
+
+    let mut file = File::create(&path).unwrap();
+    file.write_all(contents.as_bytes()).unwrap();
+
+    let mut permissions = metadata(path).unwrap().permissions();
+    permissions.set_mode(mode);
+    file.set_permissions(permissions).unwrap();
+}
+
+#[test]
+fn run_multiple_hooks_in_a_dir() {
+    let tmpdir = Temp::new_dir().unwrap();
+
+    // create two scripts so we can test the parsing of output
+    create_hook(&tmpdir.to_path_buf().join("hook1"),
+                "#!/bin/sh\necho key2=val2\necho key1=val1",
+                0o755);
+    create_hook(&tmpdir.to_path_buf().join("hook2"),
+                "#!/bin/sh\necho key2=val4\necho key1=val3",
+                0o755);
+
+    let fv = run_hooks_from_dir(&tmpdir.to_path_buf()).unwrap();
+
+    assert_eq!(fv.keys().len(), 2);
+    assert_eq!(fv.keys().collect::<Vec<_>>(), ["key1", "key2"]);
+    assert_eq!(fv["key1"], ["val1", "val3"]);
+    assert_eq!(fv["key2"], ["val2", "val4"]);
+}
+
+#[test]
+fn check_load_metadata() {
+    use std::fs::remove_file;
+
+    let tmpdir = Temp::new_dir().unwrap().to_path_buf();
+
+    let product_uid_hook = tmpdir.join(PRODUCT_UID_HOOK);
+    let version_hook = tmpdir.join(VERSION_HOOK);
+    let hardware_hook = tmpdir.join(HARDWARE_HOOK);
+    let device_identity_dir = tmpdir.join(DEVICE_IDENTITY_DIR).join("identity");
+    let device_attributes_dir = tmpdir.join(DEVICE_ATTRIBUTES_DIR).join("attributes");
+
+    let setup_metadata = || {
+        // create fake hooks to be used to validate the load
+        create_hook(&product_uid_hook,
+                    "#!/bin/sh\necho 229ffd7e08721d716163fc81a2dbaf6c90d449f0a3b009b6a2defe8a0b0d7381",
+                    0o755);
+        create_hook(&version_hook, "#!/bin/sh\necho 1.1", 0o755);
+        create_hook(&hardware_hook, "#!/bin/sh\necho board", 0o755);
+        create_hook(&device_identity_dir,
+                    "#!/bin/sh\necho id1=value1\necho id2=value2",
+                    0o755);
+        create_hook(&device_attributes_dir,
+                    "#!/bin/sh\necho attr1=attrvalue1\necho attr2=attrvalue2",
+                    0o755);
+    };
+
+    // check error with a invalid product uid
+    setup_metadata();
+    create_hook(&product_uid_hook, "#!/bin/sh\necho 123", 0o755);
+    let metadata = Metadata::new(&tmpdir);
+    assert!(metadata.is_err());
+
+    // check error when lacks product uid
+    setup_metadata();
+    remove_file(&product_uid_hook).unwrap();
+    let metadata = Metadata::new(&tmpdir);
+    assert!(metadata.is_err());
+
+    // check error when lacks device identity
+    setup_metadata();
+    remove_file(&device_identity_dir).unwrap();
+    let metadata = Metadata::new(&tmpdir);
+    assert!(metadata.is_err());
+
+    // check if is still valid without device attributes
+    setup_metadata();
+    remove_file(&device_attributes_dir).unwrap();
+    let metadata = Metadata::new(&tmpdir).unwrap();
+    assert_eq!("229ffd7e08721d716163fc81a2dbaf6c90d449f0a3b009b6a2defe8a0b0d7381",
+               metadata.product_uid);
+    assert_eq!("1.1", metadata.version);
+    assert_eq!("board", metadata.hardware);
+    assert_eq!(2, metadata.device_identity.len());
+    assert_eq!(0, metadata.device_attributes.len());
+
+    // complete metadata
+    setup_metadata();
+    let metadata = Metadata::new(&tmpdir).unwrap();
+    assert_eq!("229ffd7e08721d716163fc81a2dbaf6c90d449f0a3b009b6a2defe8a0b0d7381",
+               metadata.product_uid);
+    assert_eq!("1.1", metadata.version);
+    assert_eq!("board", metadata.hardware);
+    assert_eq!(2, metadata.device_identity.len());
+    assert_eq!(2, metadata.device_attributes.len());
+}
