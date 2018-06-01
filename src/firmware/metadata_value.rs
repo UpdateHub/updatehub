904df69af4ccdd72f812e104a9736cce588225f8
Otavio Salvador <otavio@ossystems.com.br>
2018-06-01 18:43:43 -0300

    firmware: replace to use .and_modify
    The Rust's #44733 issue is now stable, so we can rework the code to
    use the .and_modify method and make the code simple to read.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/firmware/metadata_value.rs
+++ src/firmware/metadata_value.rs
@@ -37,19 +37,9 @@ impl FromStr for MetadataValue {
 
         let mut mv = MetadataValue::default();
         for (k, v) in values {
-            // replace to use .and_modify once #44733 is closed. Code
-            // is below:
-            //   mv.entry(k)
-            //     .and_modify(|e| e.push(v.clone()))
-            //     .or_insert(vec![v]);
-            match mv.entry(k) {
-                Entry::Occupied(entry) => {
-                    entry.into_mut().push(v.clone());
-                }
-                Entry::Vacant(entry) => {
-                    entry.insert(vec![v]);
-                }
-            }
+            mv.entry(k)
+                .and_modify(|e| e.push(v.clone()))
+                .or_insert(vec![v]);
         }
 
         Ok(mv)
