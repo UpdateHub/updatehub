8a677915dae26a70e504f36749e104722e60612b
Otavio Salvador <otavio@ossystems.com.br>
2018-06-11 15:22:49 -0300

    lib.rs: Rework the importing of crates and sub-modules
    We grouped it better and reduced the size of code.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
+extern crate chrono;
 extern crate core;
-
+extern crate crypto_hash;
 extern crate easy_process;
+extern crate hex;
+extern crate parse_duration;
+extern crate rand;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_ini;
+extern crate walkdir;
 
 #[macro_use]
 extern crate failure;
@@ -10,52 +18,29 @@ extern crate failure_derive;
 #[macro_use]
 extern crate log;
 
-extern crate chrono;
-extern crate crypto_hash;
-extern crate hex;
-
-extern crate serde;
+#[macro_use]
+extern crate hyper;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_ini;
-
-#[cfg(test)]
-#[macro_use]
-extern crate serde_json;
-
 #[cfg(not(test))]
 extern crate serde_json;
 
-extern crate parse_duration;
-
-extern crate rand;
-
-#[macro_use]
-extern crate hyper;
-extern crate reqwest;
-
-extern crate walkdir;
-
 #[cfg(test)]
 extern crate mktemp;
-
 #[cfg(test)]
 extern crate mockito;
+#[cfg(test)]
+#[macro_use]
+extern crate serde_json;
 
 pub mod build_info;
-
-mod serde_helpers;
-mod update_package;
-
-pub mod runtime_settings;
-pub mod settings;
-
 pub mod client;
-
 pub mod firmware;
-
+pub mod runtime_settings;
+mod serde_helpers;
+pub mod settings;
 pub mod states;
-
+mod update_package;
 pub use failure::Error;
 
 use std::result;
