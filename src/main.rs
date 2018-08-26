736b9a80ba395f9c86e8cad1213163f141705131
Otavio Salvador <otavio@ossystems.com.br>
2018-08-26 18:02:45 -0300

    app: move cmdline parsing to main.rs
    The command line parsing will be reworked to use `structopt` then
    there is no need to a module for it.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,58 @@
 
 #[macro_use]
 extern crate log;
+extern crate app;
 extern crate stderrlog;
 extern crate updatehub;
 
-mod cmdline;
+use app::{App, Opt};
+use std::env;
+
+impl CmdLine {
+    pub fn parse_args() -> Self {
+        let mut config = CmdLine::default();
+
+        let helper = {
+            App::new("updatehub")
+                .version(env::var("CARGO_PKG_VERSION").unwrap_or_else(|_| "Unknown".to_string()))
+                .author("O.S. Systems Software LTDA.", "contact@ossystems.com.br")
+                .desc("A generic and safe Firmware Over-The-Air agent.")
+                .opt(
+                    Opt::new("debug", &mut config.debug)
+                        .short('d')
+                        .long("debug")
+                        .help("Enable debug messages"),
+                ).opt(
+                    Opt::new("quiet", &mut config.quiet)
+                        .short('q')
+                        .long("quiet")
+                        .help("Disable informative message"),
+                ).parse_args()
+        };
+
+        config
+            .check()
+            .map_err(|e| helper.help_err_exit(e, 1))
+            .unwrap() // help_err_exit already exits, so unwrap is safe here!
+    }
+
+    fn check(self) -> Result<Self, String> {
+        if self.debug && self.quiet {
+            return Err("You cannot enable 'quiet' and 'debug' at same time.".to_string());
+        }
+
+        Ok(self)
+    }
+}
+
+#[derive(Default)]
+pub struct CmdLine {
+    pub debug: bool,
+    pub quiet: bool,
+}
 
 fn run() -> updatehub::Result<()> {
-    let cmdline = cmdline::CmdLine::parse_args();
+    let cmdline = CmdLine::parse_args();
 
     stderrlog::new()
         .quiet(cmdline.quiet)
