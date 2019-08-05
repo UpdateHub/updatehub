946296ab62d7458dc122cb7c74d2aeb8550fbaf8
Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>
2019-08-05 17:45:36 -0300

    updatehub: Update use of Imxkobs internal structure and add tests
    Signed-off-by: Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>

--- updatehub/src/object/installer/imxkobs.rs
+++ updatehub/src/object/installer/imxkobs.rs
@@ -21,10 +21,10 @@ impl Installer for objects::Imxkobs {
 
     fn install(&self, download_dir: &std::path::Path) -> Result<(), failure::Error> {
         info!("'imxkobs' handler Install");
-        let mut cmd = String::from("kobs-ng init");
+        let mut cmd = String::from("kobs-ng init ");
 
         if self.padding_1k {
-            cmd += " -x"
+            cmd += "-x "
         };
 
         cmd += download_dir
@@ -36,15 +36,171 @@ impl Installer for objects::Imxkobs {
             cmd += &format!(" --search_exponent={}", self.search_exponent)
         }
 
-        if !self.chip_0_device_path.is_empty() {
-            cmd += &format!(" --chip_0_device_path={}", self.chip_0_device_path);
+        if let Some(chip_0) = &self.chip_0_device_path {
+            cmd += &format!(" --chip_0_device_path={}", chip_0.display());
         }
 
-        if !self.chip_1_device_path.is_empty() {
-            cmd += &format!(" --chip_1_device_path={}", self.chip_1_device_path);
+        if let Some(chip_1) = &self.chip_1_device_path {
+            cmd += &format!(" --chip_1_device_path={}", chip_1.display());
         }
+
+        cmd += " -v";
 
         easy_process::run(&cmd)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::object::installer::tests::create_echo_bins;
+    use pretty_assertions::assert_eq;
+    use std::{env, path::PathBuf};
+
+    fn fake_imxkobs_obj() -> objects::Imxkobs {
+        objects::Imxkobs {
+            filename: "imxkobs-filename".to_string(),
+            size: 1024,
+            sha256sum: "e3b0c44298fc1c149afb".to_string(),
+
+            install_if_different: None,
+            padding_1k: true,
+            search_exponent: 2,
+            chip_0_device_path: Some(PathBuf::from("/dev/sda1")),
+            chip_1_device_path: Some(PathBuf::from("/dev/sda2")),
+        }
+    }
+
+    #[test]
+    fn check_requirements_with_missing_binaries() {
+        let imxkobs_obj = fake_imxkobs_obj();
+
+        env::set_var("PATH", "");
+        assert!(imxkobs_obj.check_requirements().is_err());
+    }
+
+    #[test]
+    fn install_no_args() {
+        let mut imxkobs_obj = fake_imxkobs_obj();
+        imxkobs_obj.padding_1k = false;
+        imxkobs_obj.search_exponent = 0;
+        imxkobs_obj.chip_0_device_path = None;
+        imxkobs_obj.chip_1_device_path = None;
+        let download_dir = tempfile::tempdir().unwrap();
+        let source = download_dir.path().join(&imxkobs_obj.sha256sum);
+
+        let (_handle, calls) = create_echo_bins(&["kobs-ng"]).unwrap();
+
+        imxkobs_obj.check_requirements().unwrap();
+        imxkobs_obj.install(download_dir.path()).unwrap();
+
+        let expected = format!("kobs-ng init {} -v\n", source.to_str().unwrap());
+        assert_eq!(std::fs::read_to_string(calls).unwrap(), expected);
+    }
+
+    #[test]
+    fn install_padding_1k() {
+        let mut imxkobs_obj = fake_imxkobs_obj();
+        imxkobs_obj.search_exponent = 0;
+        imxkobs_obj.chip_0_device_path = None;
+        imxkobs_obj.chip_1_device_path = None;
+        let download_dir = tempfile::tempdir().unwrap();
+        let source = download_dir.path().join(&imxkobs_obj.sha256sum);
+
+        let (_handle, calls) = create_echo_bins(&["kobs-ng"]).unwrap();
+
+        imxkobs_obj.check_requirements().unwrap();
+        imxkobs_obj.install(download_dir.path()).unwrap();
+
+        let expected = format!("kobs-ng init -x {} -v\n", source.to_str().unwrap());
+        assert_eq!(std::fs::read_to_string(calls).unwrap(), expected);
+    }
+
+    #[test]
+    fn install_search_exponent() {
+        let mut imxkobs_obj = fake_imxkobs_obj();
+        imxkobs_obj.padding_1k = false;
+        imxkobs_obj.chip_0_device_path = None;
+        imxkobs_obj.chip_1_device_path = None;
+        let download_dir = tempfile::tempdir().unwrap();
+        let source = download_dir.path().join(&imxkobs_obj.sha256sum);
+
+        let (_handle, calls) = create_echo_bins(&["kobs-ng"]).unwrap();
+
+        imxkobs_obj.check_requirements().unwrap();
+        imxkobs_obj.install(download_dir.path()).unwrap();
+
+        let expected = format!(
+            "kobs-ng init {} --search_exponent={} -v\n",
+            source.to_str().unwrap(),
+            imxkobs_obj.search_exponent
+        );
+        assert_eq!(std::fs::read_to_string(calls).unwrap(), expected);
+    }
+
+    #[test]
+    fn install_chip_0() {
+        let mut imxkobs_obj = fake_imxkobs_obj();
+        imxkobs_obj.padding_1k = false;
+        imxkobs_obj.search_exponent = 0;
+        imxkobs_obj.chip_1_device_path = None;
+        let download_dir = tempfile::tempdir().unwrap();
+        let source = download_dir.path().join(&imxkobs_obj.sha256sum);
+
+        let (_handle, calls) = create_echo_bins(&["kobs-ng"]).unwrap();
+
+        imxkobs_obj.check_requirements().unwrap();
+        imxkobs_obj.install(download_dir.path()).unwrap();
+
+        let expected = format!(
+            "kobs-ng init {} --chip_0_device_path={} -v\n",
+            source.to_str().unwrap(),
+            imxkobs_obj.chip_0_device_path.unwrap().display()
+        );
+        assert_eq!(std::fs::read_to_string(calls).unwrap(), expected);
+    }
+
+    #[test]
+    fn install_chip_1() {
+        let mut imxkobs_obj = fake_imxkobs_obj();
+        imxkobs_obj.padding_1k = false;
+        imxkobs_obj.search_exponent = 0;
+        imxkobs_obj.chip_0_device_path = None;
+        let download_dir = tempfile::tempdir().unwrap();
+        let source = download_dir.path().join(&imxkobs_obj.sha256sum);
+
+        let (_handle, calls) = create_echo_bins(&["kobs-ng"]).unwrap();
+
+        imxkobs_obj.check_requirements().unwrap();
+        imxkobs_obj.install(download_dir.path()).unwrap();
+
+        let expected = format!(
+            "kobs-ng init {} --chip_1_device_path={} -v\n",
+            source.to_str().unwrap(),
+            imxkobs_obj.chip_1_device_path.unwrap().display()
+        );
+        assert_eq!(std::fs::read_to_string(calls).unwrap(), expected);
+    }
+
+    #[test]
+    fn install_all_fields() {
+        let imxkobs_obj = fake_imxkobs_obj();
+        let download_dir = tempfile::tempdir().unwrap();
+        let source = download_dir.path().join(&imxkobs_obj.sha256sum);
+
+        let (_handle, calls) = create_echo_bins(&["kobs-ng"]).unwrap();
+
+        imxkobs_obj.check_requirements().unwrap();
+        imxkobs_obj.install(download_dir.path()).unwrap();
+
+        let expected = format!(
+            "kobs-ng init -x {} --search_exponent={} --chip_0_device_path={} --chip_1_device_path={} -v\n",
+            source.to_str().unwrap(),
+            imxkobs_obj.search_exponent,
+            imxkobs_obj.chip_0_device_path.unwrap().display(),
+            imxkobs_obj.chip_1_device_path.unwrap().display()
+        );
+        assert_eq!(std::fs::read_to_string(calls).unwrap(), expected);
     }
 }
