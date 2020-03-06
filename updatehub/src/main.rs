f37efaa9e55f86682b9846e6b3ba4d5a8084eb00
Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>
2020-03-06 11:42:53 -0300

    updatehub: Finish commands on client
    Signed-off-by: Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>

--- updatehub/src/main.rs
+++ updatehub/src/main.rs
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 use argh::FromArgs;
 use slog_scope::info;
+use std::path::PathBuf;
 
 #[derive(FromArgs)]
 /// Top-level command.
@@ -14,61 +15,71 @@ struct TopLevel {
 #[derive(FromArgs)]
 #[argh(subcommand)]
 enum EntryPoints {
-    Cli(CliOptions),
+    Client(ClientOptions),
     Server(ServerOptions),
 }
 
 #[derive(FromArgs)]
-/// Cli subcommand
-#[argh(subcommand, name = "cli")]
-struct CliOptions {
+/// Client subcommand
+#[argh(subcommand, name = "client")]
+struct ClientOptions {
     #[argh(subcommand)]
-    commands: CliCommands,
+    commands: ClientCommands,
 }
 
 #[derive(FromArgs)]
 #[argh(subcommand)]
-enum CliCommands {
-    Probe(SubCommandProbe),
-    ProbeCustom(SubCommandProbeCustom),
-    Info(SubCommandInfo),
-    Log(SubCommandLog),
-    Abort(SubCommandAbort),
-    Download(SubCommandDownload),
+enum ClientCommands {
+    Info(Info),
+    Log(Log),
+    Probe(Probe),
+    AbortDownload(AbortDownload),
+    LocalInstall(LocalInstall),
+    RemoteInstall(RemoteInstall),
 }
 
 #[derive(FromArgs)]
-/// Probe subcommand
-#[argh(subcommand, name = "probe")]
-struct SubCommandProbe {}
-
-#[derive(FromArgs)]
-/// Info subcommand
+/// Fetches information about the current state of the agent
 #[argh(subcommand, name = "info")]
-struct SubCommandInfo {}
+struct Info {}
 
 #[derive(FromArgs)]
-/// Log subcommand
+/// Fetches the available log entries for the last update cycle
 #[argh(subcommand, name = "log")]
-struct SubCommandLog {}
+struct Log {}
 
 #[derive(FromArgs)]
-/// Abort subcommand
-#[argh(subcommand, name = "abort")]
-struct SubCommandAbort {}
-
-#[derive(FromArgs)]
-/// Download subcommand
-#[argh(subcommand, name = "download")]
-struct SubCommandDownload {}
-
-#[derive(FromArgs)]
-/// Probe custom subcommand
-#[argh(subcommand, name = "probe-custom")]
-struct SubCommandProbeCustom {
+/// Checks if the server has a new update for this device.
+///
+/// A custom server for the update cycle can be specified via the ´--server´
+#[argh(subcommand, name = "probe")]
+struct Probe {
     /// custom address to try probe
+    #[argh(option)]
+    server: Option<String>,
+}
+
+#[derive(FromArgs)]
+/// Abort current running download
+#[argh(subcommand, name = "abort-download")]
+struct AbortDownload {}
+
+#[derive(FromArgs)]
+/// Request agent to install a local update package
+#[argh(subcommand, name = "local-install")]
+struct LocalInstall {
+    /// path to the update package
     #[argh(positional)]
-    server: String,
+    file: PathBuf,
+}
+
+#[derive(FromArgs)]
+/// Request agent to download and install a package from a direct URL
+#[argh(subcommand, name = "remote-install")]
+struct RemoteInstall {
+    /// the URL to get the update package
+    #[argh(positional)]
+    url: String,
 }
 
 #[derive(FromArgs)]
@@ -77,7 +88,7 @@ struct SubCommandProbeCustom {
 struct ServerOptions {
     /// increase the verboseness level
     #[argh(option, short = 'v', from_str_fn(verbosity_level))]
-    verbose: Option<slog::Level>,
+    verbosity: Option<slog::Level>,
 }
 
 fn verbosity_level(value: &str) -> Result<slog::Level, String> {
@@ -86,7 +97,7 @@ fn verbosity_level(value: &str) -> Result<slog::Level, String> {
 }
 
 async fn server_main(cmd: ServerOptions) -> updatehub::Result<()> {
-    updatehub::logger::init(cmd.verbose.unwrap_or(slog::Level::Info));
+    updatehub::logger::init(cmd.verbosity.unwrap_or(slog::Level::Info));
     info!("Starting UpdateHub Agent {}", updatehub::version());
 
     let settings = updatehub::Settings::load()?;
@@ -95,19 +106,23 @@ async fn server_main(cmd: ServerOptions) -> updatehub::Result<()> {
     Ok(())
 }
 
-async fn cli_main(cmd: CliCommands) -> updatehub::Result<()> {
+async fn client_main(cmd: ClientCommands) -> updatehub::Result<()> {
     let client = sdk::Client::new("localhost:8080");
 
-    let res = match cmd {
-        CliCommands::Info(_) => client.info().await,
-        CliCommands::Probe(_) => todo!("probe"),
-        CliCommands::Log(_) => todo!("log"),
-        CliCommands::Abort(_) => todo!("abort"),
-        CliCommands::Download(_) => todo!("Download"),
-        CliCommands::ProbeCustom(input) => todo!("Probe custom {:?}", input.server),
-    };
-
-    dbg!(res).unwrap();
+    match cmd {
+        ClientCommands::Info(_) => println!("{:#?}", client.info().await),
+        ClientCommands::Log(_) => println!("{:#?}", client.log().await),
+        ClientCommands::Probe(Probe { server }) => println!("{:#?}", client.probe(server).await),
+        ClientCommands::AbortDownload(_) => println!("{:#?}", client.abort_download().await),
+        ClientCommands::LocalInstall(LocalInstall { file }) => {
+            let file =
+                if file.is_absolute() { file } else { std::env::current_dir().unwrap().join(file) };
+            println!("{:#?}", client.local_install(file).await)
+        }
+        ClientCommands::RemoteInstall(RemoteInstall { url }) => {
+            println!("{:#?}", client.remote_install(url).await)
+        }
+    }
 
     Ok(())
 }
@@ -117,7 +132,7 @@ async fn main() {
     let cmd: TopLevel = argh::from_env();
 
     let res = match cmd.entry_point {
-        EntryPoints::Cli(cli) => cli_main(cli.commands).await,
+        EntryPoints::Client(client) => client_main(client.commands).await,
         EntryPoints::Server(cmd) => server_main(cmd).await,
     };
 
