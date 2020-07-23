a57454c95c8f97c264824dc74053dc6e5c262733
Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>
2020-07-23 18:07:45 -0300

    updatehub: CommunicationState trait
    Communication is now handled as a trait for states that implement
    `StateChangeImpl`, that way inner states can also handle concurrent messages
    
    Signed-off-by: Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>

--- updatehub/src/states/machine/mod.rs
+++ updatehub/src/states/machine/mod.rs
@@ -10,6 +10,7 @@ use super::{
 };
 use async_std::{prelude::FutureExt, sync};
 use slog_scope::trace;
+use std::path::PathBuf;
 
 pub(crate) use address::{
     AbortDownloadResponse, Addr, Message, ProbeResponse, Response, StateResponse,
@@ -29,14 +30,167 @@ pub struct Context {
 }
 
 pub(super) struct Channel<T> {
-    sender: sync::Sender<T>,
-    receiver: sync::Receiver<T>,
+    pub(super) sender: sync::Sender<T>,
+    pub(super) receiver: sync::Receiver<T>,
 }
 
 impl<T> Channel<T> {
     fn new(cap: usize) -> Self {
         let (sender, receiver) = sync::channel(cap);
         Channel { sender, receiver }
+    }
+}
+
+impl CommunicationState for State {}
+
+#[async_trait::async_trait]
+pub(super) trait CommunicationState: StateChangeImpl {
+    async fn handle_communication(
+        &self,
+        msg: address::Message,
+        responder: sync::Sender<Result<address::Response>>,
+        context: &mut Context,
+    ) -> Option<State> {
+        trace!("Received external request: {:?}", msg);
+
+        let res = match msg {
+            address::Message::Info => {
+                let state = self.name().to_owned();
+                Ok((
+                    address::Response::Info(sdk::api::info::Response {
+                        state,
+                        version: crate::version().to_string(),
+                        config: context.settings.0.clone(),
+                        firmware: context.firmware.0.clone(),
+                        runtime_settings: context.runtime_settings.0.clone(),
+                    }),
+                    None,
+                ))
+            }
+            address::Message::Probe(custom_server) => self
+                .handle_probe(context, custom_server)
+                .await
+                .map(|(res, st)| (address::Response::Probe(res), st)),
+            address::Message::AbortDownload => self
+                .handle_abort_download(context)
+                .await
+                .map(|(res, st)| (address::Response::AbortDownload(res), st)),
+            address::Message::LocalInstall(update_file) => self
+                .handle_local_install(context, update_file)
+                .await
+                .map(|(res, st)| (address::Response::LocalInstall(res), st)),
+            address::Message::RemoteInstall(url) => self
+                .handle_remote_install(context, url)
+                .await
+                .map(|(res, st)| (address::Response::RemoteInstall(res), st)),
+        };
+
+        match res {
+            Ok((response, state)) => {
+                responder.send(Ok(response)).await;
+                state
+            }
+            Err(e) => {
+                responder.send(Err(e)).await;
+                None
+            }
+        }
+    }
+
+    async fn handle_probe(
+        &self,
+        context: &mut Context,
+        custom_server: Option<String>,
+    ) -> Result<(address::ProbeResponse, Option<State>)> {
+        use chrono::Utc;
+        use cloud::api::ProbeResponse;
+
+        if !self.is_preemptive_state() {
+            return Ok((address::ProbeResponse::Busy(self.name().to_owned()), None));
+        }
+
+        if let Some(server_address) = custom_server {
+            context.runtime_settings.set_custom_server_address(&server_address);
+        }
+
+        match crate::CloudClient::new(&context.server_address())
+            .probe(context.runtime_settings.retries() as u64, context.firmware.as_cloud_metadata())
+            .await?
+        {
+            ProbeResponse::ExtraPoll(s) => Ok((address::ProbeResponse::Delayed(s), None)),
+
+            ProbeResponse::NoUpdate => {
+                context.waker.sender.send(()).await;
+
+                // Store timestamp of last polling
+                context.runtime_settings.set_last_polling(Utc::now())?;
+                Ok((address::ProbeResponse::Unavailable, Some(State::EntryPoint(EntryPoint {}))))
+            }
+
+            ProbeResponse::Update(package, sign) => {
+                context.waker.sender.send(()).await;
+
+                // Store timestamp of last polling
+                context.runtime_settings.set_last_polling(Utc::now())?;
+                Ok((
+                    address::ProbeResponse::Available,
+                    Some(State::Validation(Validation { package, sign })),
+                ))
+            }
+        }
+    }
+
+    async fn handle_abort_download(
+        &self,
+        _: &Context,
+    ) -> Result<(address::AbortDownloadResponse, Option<State>)> {
+        if self.is_handling_download() {
+            Ok((
+                address::AbortDownloadResponse::RequestAccepted,
+                Some(State::EntryPoint(EntryPoint {})),
+            ))
+        } else {
+            Ok((address::AbortDownloadResponse::InvalidState, None))
+        }
+    }
+
+    async fn handle_local_install(
+        &self,
+        context: &Context,
+        update_file: PathBuf,
+    ) -> Result<(address::StateResponse, Option<State>)> {
+        let name = self.name().to_owned();
+        if self.is_preemptive_state() {
+            crate::logger::start_memory_logging();
+            context.waker.sender.send(()).await;
+
+            Ok((
+                address::StateResponse::RequestAccepted(name),
+                Some(State::PrepareLocalInstall(PrepareLocalInstall { update_file })),
+            ))
+        } else {
+            Ok((address::StateResponse::InvalidState(name), None))
+        }
+    }
+
+    async fn handle_remote_install(
+        &self,
+        context: &Context,
+        url: String,
+    ) -> Result<(address::StateResponse, Option<State>)> {
+        let name = self.name().to_owned();
+
+        if self.is_preemptive_state() {
+            crate::logger::start_memory_logging();
+            context.waker.sender.send(()).await;
+
+            Ok((
+                address::StateResponse::RequestAccepted(name),
+                Some(State::DirectDownload(DirectDownload { url })),
+            ))
+        } else {
+            Ok((address::StateResponse::InvalidState(name), None))
+        }
     }
 }
 
@@ -133,129 +287,20 @@ impl StateMachine {
 
     async fn consume_pending_communication(&mut self) {
         while let Ok((msg, responder)) = self.context.communication.receiver.try_recv() {
-            self.handle_communication(msg, responder).await;
+            if let Some(new_state) =
+                self.state.handle_communication(msg, responder, &mut self.context).await
+            {
+                self.state = new_state;
+            }
         }
     }
 
     async fn await_communication(&mut self) {
         while let Ok((msg, responder)) = self.context.communication.receiver.recv().await {
-            self.handle_communication(msg, responder).await;
-        }
-    }
-
-    async fn handle_communication(
-        &mut self,
-        msg: address::Message,
-        responder: sync::Sender<Result<address::Response>>,
-    ) {
-        trace!("Received external request: {:?}", msg);
-
-        let response = match msg {
-            address::Message::Info => {
-                let state = self.state.name().to_owned();
-                Ok(address::Response::Info(sdk::api::info::Response {
-                    state,
-                    version: crate::version().to_string(),
-                    config: self.context.settings.0.clone(),
-                    firmware: self.context.firmware.0.clone(),
-                    runtime_settings: self.context.runtime_settings.0.clone(),
-                }))
-            }
-            address::Message::Probe(custom_server) => {
-                self.handle_probe_request(custom_server).await.map(|r| address::Response::Probe(r))
-            }
-            address::Message::AbortDownload => {
-                if self.state.is_handling_download() {
-                    self.state = State::EntryPoint(EntryPoint {});
-                    Ok(address::Response::AbortDownload(
-                        address::AbortDownloadResponse::RequestAccepted,
-                    ))
-                } else {
-                    Ok(address::Response::AbortDownload(
-                        address::AbortDownloadResponse::InvalidState,
-                    ))
-                }
-            }
-            address::Message::LocalInstall(update_file) => {
-                let state = self.state.name().to_owned();
-
-                if self.state.is_preemptive_state() {
-                    crate::logger::start_memory_logging();
-                    self.context.waker.sender.send(()).await;
-
-                    self.state = State::PrepareLocalInstall(PrepareLocalInstall { update_file });
-
-                    Ok(address::Response::LocalInstall(address::StateResponse::RequestAccepted(
-                        state,
-                    )))
-                } else {
-                    Ok(address::Response::LocalInstall(address::StateResponse::InvalidState(state)))
-                }
-            }
-            address::Message::RemoteInstall(url) => {
-                let state = self.state.name().to_owned();
-
-                if self.state.is_preemptive_state() {
-                    crate::logger::start_memory_logging();
-                    self.context.waker.sender.send(()).await;
-
-                    self.state = State::DirectDownload(DirectDownload { url });
-
-                    Ok(address::Response::RemoteInstall(address::StateResponse::RequestAccepted(
-                        state,
-                    )))
-                } else {
-                    Ok(address::Response::RemoteInstall(address::StateResponse::InvalidState(
-                        state,
-                    )))
-                }
-            }
-        };
-
-        responder.send(response).await;
-    }
-
-    async fn handle_probe_request(
-        &mut self,
-        custom_server: Option<String>,
-    ) -> Result<address::ProbeResponse> {
-        use chrono::Utc;
-        use cloud::api::ProbeResponse;
-
-        if !self.state.is_preemptive_state() {
-            let state = self.state.name().to_owned();
-            return Ok(address::ProbeResponse::Busy(state));
-        }
-
-        if let Some(server_address) = custom_server {
-            self.context.runtime_settings.set_custom_server_address(&server_address);
-        }
-
-        match crate::CloudClient::new(&self.context.server_address())
-            .probe(
-                self.context.runtime_settings.retries() as u64,
-                self.context.firmware.as_cloud_metadata(),
-            )
-            .await?
-        {
-            ProbeResponse::ExtraPoll(s) => Ok(address::ProbeResponse::Delayed(s)),
-
-            ProbeResponse::NoUpdate => {
-                self.context.waker.sender.send(()).await;
-
-                // Store timestamp of last polling
-                self.context.runtime_settings.set_last_polling(Utc::now())?;
-                self.state = State::EntryPoint(EntryPoint {});
-                Ok(address::ProbeResponse::Unavailable)
-            }
-
-            ProbeResponse::Update(package, sign) => {
-                self.context.waker.sender.send(()).await;
-
-                // Store timestamp of last polling
-                self.context.runtime_settings.set_last_polling(Utc::now())?;
-                self.state = State::Validation(Validation { package, sign });
-                Ok(address::ProbeResponse::Available)
+            if let Some(new_state) =
+                self.state.handle_communication(msg, responder, &mut self.context).await
+            {
+                self.state = new_state;
             }
         }
     }
