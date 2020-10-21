71125b3171baf2396c433909466ba88d382bdad3
asakiz <asakizin@gmail.com>
2020-10-21 17:23:09 -0300

    updatehub-sdk: doc: Add doc for the crate
    Signed-off-by: asakiz <asakizin@gmail.com>

--- updatehub-sdk/src/lib.rs
+++ updatehub-sdk/src/lib.rs
@@ -2,6 +2,18 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+//! The `updatehub-sdk` crate is used to communicate with UpdateHub Agent.
+//!
+//! When running an agent instance, the API provides some methods
+//! for communicating with UpdateHub:
+//!
+//! - [abort_download](Client::abort_download)
+//! - [info](Client::info)
+//! - [local_install](Client::local_install)
+//! - [log](Client::log)
+//! - [probe](Client::probe)
+//! - [remote_install](Client::remote_install)
+
 pub mod api;
 mod client;
 mod error;
