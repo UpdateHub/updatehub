UpdateHub/updatehub

commit 56544d95746a0c582eb24ee235e0c39983b94c58

updatehub: Call state_change_callback for Probe state
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

updatehub/src/states/mod.rs
@@ -250,7 +250,7 @@ impl State {
             State::Park(s) => s.handle(context).await,
             State::EntryPoint(s) => s.handle(context).await,
             State::Poll(s) => s.handle(context).await,
-            State::Probe(s) => s.handle(context).await,
+            State::Probe(s) => s.handle_with_callback(context).await,
             State::Validation(s) => s.handle(context).await,
             State::DirectDownload(s) => s.handle(context).await,
             State::PrepareLocalInstall(s) => s.handle(context).await,

updatehub/src/states/probe.rs
@@ -4,7 +4,7 @@
 
 use super::{
     machine::{self, Context},
-    EntryPoint, Result, State, StateChangeImpl, Validation,
+    CallbackReporter, EntryPoint, Result, State, StateChangeImpl, Validation,
 };
 use chrono::{Duration, Utc};
 use cloud::api::ProbeResponse;
@@ -12,6 +12,8 @@ use slog_scope::{error, info};
 
 #[derive(Debug)]
 pub(super) struct Probe;
+
+impl CallbackReporter for Probe {}
 
 /// Implements the state change for State<Probe>.
 #[async_trait::async_trait]

updatehub/tests/successful_integration_test.rs
@@ -329,7 +329,7 @@ fn correct_config_update_polling() {
 #[test]
 fn correct_config_statechange_callback() {
     let state_change_script = r#"#! /bin/bash
-echo "cancel"
+[ "$1" = "download" ] && echo "cancel" || echo
 "#;
 
     let (mut session, setup) = Settings::default()
@@ -391,7 +391,7 @@ echo "cancel"
 #[test]
 fn correct_config_error_state_callback() {
     let state_change_script = r#"#! /bin/bash
-echo "cancel"
+[ "$1" = "error" ] && echo "cancel" || echo
 "#;
 
     let (mut session, setup) = Settings::default()

commit bc1167a5082eb3af189504503808cf93789d95f3

updatehub: states: probe: set last polling when state is cancelled
We need to implement a custom transition handler as we need to set the
last polling time or we loop forever as polling interval will not be
respected.

Fixes: 56544d9 ("updatehub: Call state_change_callback for Probe state")
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

updatehub/src/states/mod.rs
@@ -84,6 +84,14 @@ trait StateChangeImpl {
 
 #[async_trait(?Send)]
 trait CallbackReporter: Sized + StateChangeImpl {
+    async fn handle_on_transition_cancel(&self, _context: &mut machine::Context) -> Result<()> {
+        Ok(())
+    }
+
+    async fn handle_on_error(&self, _context: &mut machine::Context) -> Result<()> {
+        Ok(())
+    }
+
     async fn handle_with_callback(
         self,
         context: &mut machine::Context,
@@ -92,13 +100,17 @@ trait CallbackReporter: Sized + StateChangeImpl {
             firmware::state_change_callback(&context.settings.firmware.metadata, self.name());
 
         match transition {
-            Ok(Transition::Continue) => Ok(self.handle(context).await?),
+            Ok(Transition::Continue) => return self.handle(context).await,
             Ok(Transition::Cancel) => {
                 info!(
                     "cancelling transition to '{}' due to state change callback request",
                     self.name()
                 );
-                Ok((State::EntryPoint(EntryPoint {}), machine::StepTransition::Immediate))
+
+                self.handle_on_transition_cancel(context).await
+                    .unwrap_or_else(|e| error!("failed calling specialized handler for cancelling transition to '{}' as state change callback has failed with: {}",
+                                               self.name(),
+                                               e));
             }
             Err(e) => {
                 error!(
@@ -106,9 +118,15 @@ trait CallbackReporter: Sized + StateChangeImpl {
                     self.name(),
                     e
                 );
-                Ok((State::EntryPoint(EntryPoint {}), machine::StepTransition::Immediate))
+
+                self.handle_on_error(context).await
+                    .unwrap_or_else(|e| error!("failed calling specialized handler for transition error to '{}' as state change callback has failed with: {}",
+                                               self.name(),
+                                               e));
             }
         }
+
+        Ok((State::EntryPoint(EntryPoint {}), machine::StepTransition::Immediate))
     }
 }
 

updatehub/src/states/probe.rs
@@ -14,7 +14,23 @@ use slog_scope::{error, info};
 #[derive(Debug)]
 pub(super) struct Probe;
 
-impl CallbackReporter for Probe {}
+#[async_trait::async_trait(?Send)]
+impl CallbackReporter for Probe {
+    async fn handle_on_transition_cancel(&self, context: &mut machine::Context) -> Result<()> {
+        // Set the last polling time or we loop forever as polling interval will not be
+        // respected.
+        context
+            .runtime_settings
+            .set_last_polling(Utc::now())
+            .log_error_msg("unable to update last polling to runtime settings")?;
+
+        Ok(())
+    }
+
+    async fn handle_on_error(&self, context: &mut machine::Context) -> Result<()> {
+        self.handle_on_transition_cancel(context).await
+    }
+}
 
 /// Implements the state change for State<Probe>.
 #[async_trait::async_trait(?Send)]